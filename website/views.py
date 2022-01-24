c71def4301bfadcf7f329eeda6513169e4419c7e
Sarguhl <52204356+sarguhl@users.noreply.github.com>
2022-01-25 00:22:03 +0100

    Edit Funktion Zwei

--- website/views.py
+++ website/views.py
@@ -7,19 +7,15 @@ import random
 
 views = Blueprint('views', __name__)
 
-def step_calculator(zahl, ware_preis, letzte_preis):
-    output_text = 0
+def step_calculator(zahl, ware_preis):
     try:
-        ergebnis = zahl * float(ware_preis)
+        ergebnis = zahl + int(ware_preis)
         if ergebnis == 0:
-            output_text = letzte_preis
-            return output_text
+            return ergebnis
         else:
-            output_text = round(ergebnis, 2)
-            return output_text
+            return ergebnis
     except ValueError:
-        output_text = letzte_preis
-        return output_text
+        return ergebnis
 
 def leergut_berechnung():
     pass
@@ -86,34 +82,28 @@ def funktion_one():
 
 @views.route("/funktion-zwei", methods=["GET","POST"])
 def funktion_two():
-    letzte_preis = 0
-    output_text = 0
     ware_preis = 0
 
     if request.method == "POST":
         ware_preis = request.form.get("ware_preis")
 
         if request.form.get('submit_button', False) == 'x1':
-            output_text = step_calculator(1,ware_preis, letzte_preis)
+            ware_preis = step_calculator(1,ware_preis)
 
         elif request.form.get('submit_button', False) == 'x2':
-            output_text = step_calculator(2,ware_preis, letzte_preis)
-
-        elif request.form.get('submit_button', False) == 'x3':
-            output_text = step_calculator(3,ware_preis, letzte_preis)
+            ware_preis = step_calculator(2,ware_preis)
 
         elif request.form.get('submit_button', False) == 'x5':
-            output_text = step_calculator(5,ware_preis, letzte_preis)
+            ware_preis = step_calculator(5,ware_preis)
 
         elif request.form.get('submit_button', False) == 'x10':
-            output_text = step_calculator(10,ware_preis, letzte_preis)
+            ware_preis = step_calculator(10,ware_preis)
         
         elif request.form.get('submit_button', False) == "clear":
             ware_preis = 0
-            output_text = 0
 
 
-    return render_template("funktion_two.html", ergebnis=output_text, ware_preis=ware_preis)
+    return render_template("funktion_two.html", ergebnis=ware_preis, ware_preis=ware_preis)
 
 @views.route("/funktion-drei", methods=["GET","POST"])
 def funktion_three():
